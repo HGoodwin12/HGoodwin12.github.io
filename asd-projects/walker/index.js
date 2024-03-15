ed50e4ea940292deee6f4c1d8dce86c22906f3de
HGoodwin12 <hcg4379976@lpssonline.com>
2024-03-15 12:59:04 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -7,16 +7,29 @@ function runProgram(){
   //////////////////////////// SETUP /////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
+  var KEY = {
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,
+    ENTER: 13
+  };
+  var walker = {
+    positionX: 0,
+    positionY: 0,
+    speedX: 0,
+    speedY: 0
+  };
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
   // Game Item Objects
-
+  
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,15 +40,30 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    repositionGameItem()
+    redrawGameItem()
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown(event) {
+    if (event.which === KEY.LEFT) {
+      console.log("left pressed");
+      walker.speedX = -5;
+    }
+    if (event.which === KEY.RIGHT) {
+      console.log("right pressed");
+      walker.speedX = 5;
+    }
+    if (event.which === KEY.UP) {
+      console.log("up pressed");
+      walker.speedY = 5;
+    }
+    if (event.which === KEY.DOWN) {
+      console.log("down pressed");
+      walker.speedY = -5;
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -49,6 +77,18 @@ function runProgram(){
 
     // turn off event handlers
     $(document).off();
+  }
+
+  function repositionGameItem() {
+    positionX += speedX; // update the position of the box along the x-axis
+    positionY += speedY; // update the position of the box along the x-axis
+  }
+  
+  function redrawGameItem() {
+    $("#walker").css("left", positionX); // draw the walker in the new location, positionX pixels away from the "left"
+    $("#walker").css("right", positionX); // draw the walker in the new location, positionX pixels away from the "right"
+    $("#walker").css("up", positionY); // draw the walker in the new location, positionY pixels away from the "up"
+    $("#walker").css("down", positionY); // draw the walker in the new location, positionY pixels away from the "down"
   }
   
 }
